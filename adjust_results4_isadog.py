cbd592d77ca34957f5131efecea23bd5c716fd12
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2021-02-09 16:30:41 -0800

    Update adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -31,7 +31,7 @@
 #           label isn't a dog.
 #
 ##
-# TODO 4: Define adjust_results4_isadog function below, specifically replace the None
+# DONE 4: Define adjust_results4_isadog function below, specifically replace the None
 #       below by the function definition of the adjust_results4_isadog function. 
 #       Notice that this function doesn't return anything because the 
 #       results_dic dictionary that is passed into the function is a mutable 
@@ -81,13 +81,13 @@ def adjust_results4_isadog(results_dic, dogfile):
         # processing line and adding dognames to dognames_dic with while loop
         while line != "":
 
-            # TODO: 4a. REPLACE pass with CODE to remove the newline character
+            # DONE: 4a. REPLACE pass with CODE to remove the newline character
             #           from the variable line  
             #
             # Process line by striping newline from line
             line = line.rstrip()
 
-            # TODO: 4b. REPLACE pass with CODE to check if the dogname(line) 
+            # DONE: 4b. REPLACE pass with CODE to check if the dogname(line) 
             #          exists within dognames_dic, then if the dogname(line) 
             #          doesn't exist within dognames_dic then add the dogname(line) 
             #          to dognames_dic as the 'key' with the 'value' of 1. 
@@ -122,7 +122,7 @@ def adjust_results4_isadog(results_dic, dogfile):
             if results_dic[key][1] in dognames_dic:
                 results_dic[key].extend((1, 1)) #777up to this point,the scenario where PetLable IS dog AND ClassiferLable IS dog is taken care of
 
-            # TODO: 4c. REPLACE pass BELOW with CODE that adds the following to
+            # DONE: 4c. REPLACE pass BELOW with CODE that adds the following to
             #           results_dic dictionary for the key indicated by the 
             #           variable key - append (1,0) to the value using 
             #           the extend list function. This indicates
@@ -135,7 +135,7 @@ def adjust_results4_isadog(results_dic, dogfile):
 
         # Pet Image Label IS NOT a Dog image (e.g. NOT found in dognames_dic)
         else:
-            # TODO: 4d. REPLACE pass BELOW with CODE that adds the following to
+            # DONE: 4d. REPLACE pass BELOW with CODE that adds the following to
             #           results_dic dictionary for the key indicated by the 
             #           variable key - append (0,1) to the value uisng
             #           the extend list function. This indicates
@@ -146,7 +146,7 @@ def adjust_results4_isadog(results_dic, dogfile):
             if results_dic[key][1] in dognames_dic:
                 results_dic[key].extend((0, 1))
 
-            # TODO: 4e. REPLACE pass BELOW with CODE that adds the following to
+            # DONE: 4e. REPLACE pass BELOW with CODE that adds the following to
             #           results_dic dictionary for the key indicated by the 
             #           variable key - append (0,0) to the value using the 
             #           extend list function. This indicates
