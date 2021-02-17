7b40a9e67792e5e919b5a9df5b70a9c6039cb460
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2021-02-16 16:01:40 -0800

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -26,7 +26,7 @@
 #         This function does not output anything other than printing a summary
 #         of the final results.
 ##
-# TODO 6: Define print_results function below, specifically replace the None
+# DONE 6: Define print_results function below, specifically replace the None
 #       below by the function definition of the print_results function. 
 #       Notice that this function doesn't to return anything because it  
 #       prints a summary of the results using results_dic and results_stats_dic
@@ -68,7 +68,7 @@ def print_results(results_dic, results_stats_dic, model,
     print("{:20}: {:3d}".format('N Images', results_stats_dic['n_images']))
     print("{:20}: {:3d}".format('N Dog Images', results_stats_dic['n_dogs_img']))
 
-    # TODO: 6a. REPLACE print("") with CODE that prints the text string 
+    # DONE: 6a. REPLACE print("") with CODE that prints the text string 
     #          'N Not-Dog Images' and then the number of NOT-dog images 
     #          that's accessed by key 'n_notdogs_img' using dictionary 
     #          results_stats_dic
@@ -79,7 +79,7 @@ def print_results(results_dic, results_stats_dic, model,
     # Prints summary statistics (percentages) on Model Run
     print(" ")
     for key in results_stats_dic:
-        # TODO: 6b. REPLACE pass with CODE that prints out all the percentages 
+        # DONE: 6b. REPLACE pass with CODE that prints out all the percentages 
         #           in the results_stats_dic dictionary. Recall that all 
         #           percentages in results_stats_dic have 'keys' that start with 
         #           the letter p. You will need to write a conditional 
@@ -104,7 +104,7 @@ def print_results(results_dic, results_stats_dic, model,
         # process through results dict, printing incorrectly classified dogs
         for key in results_dic:
 
-            # TODO: 6c. REPLACE pass with CODE that prints out the pet label 
+            # DONE: 6c. REPLACE pass with CODE that prints out the pet label 
             #           and the classifier label from results_dic dictionary    
             #           ONLY when the classifier function (classifier label) 
             #           misclassified dogs specifically: 
