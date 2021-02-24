9d9f0003f1c3d24da5ae855fe7e903c16053d340
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2021-02-23 17:21:02 -0800

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -26,7 +26,7 @@
 #         This function does not output anything other than printing a summary
 #         of the final results.
 ##
-# DONE6: Define print_results function below, specifically replace the None
+# DONE 6: Define print_results function below, specifically replace the None
 #       below by the function definition of the print_results function. 
 #       Notice that this function doesn't to return anything because it  
 #       prints a summary of the results using results_dic and results_stats_dic
